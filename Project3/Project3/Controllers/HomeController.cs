f9e3082d5966e634aaf61c81ce0fbb38824c8a4c
SS <simeon.strydom@gmail.com>
2021-12-06 01:36:03 +0200

    Previous Project update
    Added previous projects.
    Added carousel with page jumps

--- Project3/Project3/Controllers/HomeController.cs
+++ Project3/Project3/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Project3.Models;
 
@@ -10,6 +12,13 @@ namespace Project3.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IHostingEnvironment _hostingEnv;
+
+        public HomeController(IHostingEnvironment hostingEnv)
+        {
+            _hostingEnv = hostingEnv;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -39,5 +48,16 @@ namespace Project3.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [HttpGet("DownloadCV")]
+        public async Task<IActionResult> DownloadCV()
+        {
+            string path = Path.Combine(_hostingEnv.WebRootPath, "CV OF SIMEON STRYDOM.pdf");
+            byte[] bytes = System.IO.File.ReadAllBytes(path);
+            return File(bytes, "application/octet-stream", "CV OF SIMEON STRYDOM.pdf");
+        }
     }
+
+
+    // http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=440&key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX&steamid=76561197972495328
 }
